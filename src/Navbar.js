dbf2a52bc1e3d3f82c7a2dfbde42f8344675fe54
Arapu Trevor <araputrevor2@gmail.com>
2023-01-02 16:10:37 +0300

    number one

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h2>The Blog</h2>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create" style = {{ 
+                    color: "White",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "6px"
+                 }} >New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
